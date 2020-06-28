46bd710632961274e42cd79c60671c6404993dcd
Mo Lin <molin@wanchain.org>
2020-06-28 17:41:45 +0800

    update ui.

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ Wandora Box had $WANDORA_AMOUNT$ wan trade in last 24 hours.
 const jacksPotSC = "0x76b074d91f546914c6765ef81cbdc6f9c7da5685";
 
 async function getJacksPotInfos() {
-    let msg = messageModel;
+    let ret = {};
 
     while (true) {
         if (isSwitchFinish()) {
@@ -64,8 +64,7 @@ async function getJacksPotInfos() {
         }
         await sleep(100);
     }
-
-    msg=msg.replace("$DATE$", new Date().toISOString().split('T')[0]);
+    ret.date = new Date().toISOString().split('T')[0];
 
     let web3 = getWeb3();
     let sc = new web3.eth.Contract(jackspotAbi, jacksPotSC);
@@ -80,8 +79,8 @@ async function getJacksPotInfos() {
 
     let totalPool = (Number(web3.utils.fromWei(poolInfo.delegatePool)) + Number(web3.utils.fromWei(poolInfo.demandDepositPool)) + Number(web3.utils.fromWei(poolInfo.prizePool))).toFixed(1);
     let pricePool = Number(web3.utils.fromWei(poolInfo.prizePool)).toFixed(1);
-    msg=msg.replace("$TOTAL_POOL$", totalPool);
-    msg=msg.replace("$PRIZE_POOL$", pricePool);
+    ret.totalPool = totalPool;
+    ret.prizePool = pricePool;
 
     let winCode = 0;
     let winCount = 0;
@@ -90,9 +89,9 @@ async function getJacksPotInfos() {
     winCode = settleEvents[settleEvents.length - 1].returnValues.winnerCode;
     let paid_prize = Number(web3.utils.fromWei(settleEvents[settleEvents.length - 1].returnValues.prizePool)).toFixed(1);
     
-    msg=msg.replace('$WIN_NUMBER$', winCode);
-    msg=msg.replace('$WINNERS$', winCount);
-    msg=msg.replace('$PAID_PRIZE$', paid_prize);
+    ret.winCode = winCode;
+    ret.winCount = winCount;
+    ret.paidPrize = paid_prize;
 
 
     let playerData = [];
@@ -139,12 +138,10 @@ async function getJacksPotInfos() {
       }
     }
 
-    msg=msg.replace("$TOTAL_TICKETS$", tickets.length.toString());
-    msg=msg.replace("$TOTAL_PLAYER$", playerData.length.toString());
+    ret.tickets = tickets.length;
+    ret.players = playerData.length;
 
-    console.log('msg', msg);
-
-    return msg;
+    return ret;
 }
 
 getJacksPotInfos();
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,53 @@
 import React from "react";
 import styles from './index.css';
-import {getJacksPotInfos} from "../index";
+import { getJacksPotInfos } from "../index";
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {msg:"Loading..."};
+    this.state = {
+      date: "Loading...",
+      prizePool: 0,
+      totalPool: 0,
+      players: 0,
+      tickets: 0,
+      winCode: 0,
+      paidPrize: 0,
+      winCount: 0
+    };
   }
   async componentDidMount() {
     let ret = await getJacksPotInfos();
-    this.setState({msg: ret});
+    this.setState({
+      date: ret.date,
+      prizePool: ret.prizePool,
+      totalPool: ret.totalPool,
+      players: ret.players,
+      tickets: ret.tickets,
+      winCode: ret.winCode,
+      paidPrize: ret.paidPrize,
+      winCount: ret.winCount
+    });
   }
 
   render() {
     return (
       <div className={styles.normal}>
-        <h3><pre>{this.state.msg}</pre></h3>
+        <div className={styles.title}>万维链DApp简报之</div>
+        <div className={styles.title2}>Jack's Pot</div>
+        <div className={styles.date}>-- {this.state.date}</div>
+        <div className={styles.subTitle}>最新情况：</div>
+        <div className={styles.subTitle2}>上局结果：</div>
+        <div className={styles.t1}>奖&#8194;金&#8194;池：{this.state.prizePool} WAN</div>
+        <div className={styles.t2}>总资金池：{this.state.totalPool} WAN</div>
+        <div className={styles.t3}>当前人数：{this.state.players}</div>
+        <div className={styles.t4}>号码覆盖：{this.state.tickets}</div>
+        <div className={styles.t5}>中奖号码：{this.state.winCode}</div>
+        <div className={styles.t6}>奖&#12288;&#12288;金：{this.state.paidPrize} WAN</div>
+        <div className={styles.t7}>中奖人数：{this.state.winCount}</div>
+        <div className={styles.t8}> - 欢迎使用轻钱包参与Jack's Pot无损彩票，赢取大额奖金</div>
+        <div className={styles.t9}> - https://wanchain.org/getstarted</div>
       </div>
     );
   }
